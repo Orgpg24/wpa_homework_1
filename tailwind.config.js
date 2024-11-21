44e2991d6dacf888d9d43123e20e76b068cb2da4
Orgpg24 <your-email@example.com>
2024-11-21 14:37:58 +0630

    New add

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    './src/**/*.{html,js}',
+    './node_modules/flowbite/**/*.js'
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'text': '#06080a',
+        'background': '#f7f8fb',
+        'primary': '#4d75b3',
+        'secondary': '#a0b8de',
+        'accent': '#7199d9',
+        customBlue: '#D9E8EF',
+      }
+
+    }
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ]
 }
